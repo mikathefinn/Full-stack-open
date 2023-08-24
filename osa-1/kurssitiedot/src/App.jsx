8290c77ff125751c67b74e82ea571b74367fb1c6
mikathefinn <mikathefinn@gmail.com>
2023-08-24 13:26:59 +0300

    variables to objects

--- osa-1/kurssitiedot/src/App.jsx
+++ osa-1/kurssitiedot/src/App.jsx
@@ -1,11 +1,17 @@
 const App = () => {
   const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React ';
-  const exercises1 = 10;
-  const part2 = 'Using props to pass data ';
-  const exercises2 = 7;
-  const part3 = 'State of a component ';
-  const exercises3 = 14;
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10,
+  };
+  const part2 = {
+    name: 'Using props to pass data ',
+    exercises: 7,
+  };
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14,
+  };
 
   const Header = () => {
     return (
@@ -29,9 +35,9 @@ const App = () => {
   const Content = () => {
     return (
       <>
-        <Part part={part1} exc={exercises1} />
-        <Part part={part2} exc={exercises2} />
-        <Part part={part3} exc={exercises3} />
+        <Part part={part1.name} exc={part1.exercises} />
+        <Part part={part2.name} exc={part2.exercises} />
+        <Part part={part3.name} exc={part3.exercises} />
       </>
     );
   };
@@ -39,7 +45,10 @@ const App = () => {
   const Total = () => {
     return (
       <>
-        <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+        <p>
+          Number of exercises{' '}
+          {part1.exercises + part2.exercises + part3.exercises}
+        </p>
       </>
     );
   };
