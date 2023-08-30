85273086478910056ea11e8187b8a3f8b8dbc9a2
mikathefinn <mikathefinn@gmail.com>
2023-08-30 10:40:58 +0300

    changed array to individual objects

--- osa-1/kurssitiedot/src/App.jsx
+++ osa-1/kurssitiedot/src/App.jsx
@@ -1,19 +1,18 @@
 const App = () => {
   const course = 'Half Stack application development';
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10,
-    },
-    {
-      name: 'Using props to pass data ',
-      exercises: 7,
-    },
-    {
-      name: 'State of a component',
-      exercises: 14,
-    },
-  ];
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10,
+  };
+  const part2 = {
+    name: 'Using props to pass data ',
+    exercises: 7,
+  };
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14,
+  };
+
   const Header = () => {
     return (
       <>
@@ -35,9 +34,9 @@ const App = () => {
   const Content = () => {
     return (
       <>
-        <Part part={parts[0]} />
-        <Part part={parts[1]} />
-        <Part part={parts[2]} />
+        <Part part={part1} />
+        <Part part={part2} />
+        <Part part={part3} />
       </>
     );
   };
@@ -47,7 +46,7 @@ const App = () => {
       <>
         <p>
           Number of exercises{' '}
-          {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+          {part1.exercises + part2.exercises + part3.exercises}
         </p>
       </>
     );
