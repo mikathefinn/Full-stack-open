8173492df21fed1e27e34f81c42ff392ab08850f
mikathefinn <mikathefinn@gmail.com>
2023-08-30 11:02:31 +0300

    const and array to one object

--- osa-1/kurssitiedot/src/App.jsx
+++ osa-1/kurssitiedot/src/App.jsx
@@ -1,22 +1,26 @@
 const App = () => {
-  const course = 'Half Stack application development';
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10,
-  };
-  const part2 = {
-    name: 'Using props to pass data ',
-    exercises: 7,
-  };
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14,
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data ',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ],
   };
 
   const Header = () => {
     return (
       <>
-        <h1>{course}</h1>
+        <h1>{course.name}</h1>
       </>
     );
   };
@@ -34,9 +38,9 @@ const App = () => {
   const Content = () => {
     return (
       <>
-        <Part part={part1} />
-        <Part part={part2} />
-        <Part part={part3} />
+        <Part part={course.parts[0]} />
+        <Part part={course.parts[1]} />
+        <Part part={course.parts[2]} />
       </>
     );
   };
@@ -46,7 +50,9 @@ const App = () => {
       <>
         <p>
           Number of exercises{' '}
-          {part1.exercises + part2.exercises + part3.exercises}
+          {course.parts[0].exercises +
+            course.parts[1].exercises +
+            course.parts[2].exercises}
         </p>
       </>
     );
