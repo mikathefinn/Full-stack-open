69756c779f59dfaf763ea16189a230d92100a5ee
mikathefinn <mikathefinn@gmail.com>
2023-10-11 14:25:10 +0300

    1.5 done properly -- I think

--- osa-1/kurssitiedot/src/App.jsx
+++ osa-1/kurssitiedot/src/App.jsx
@@ -3,62 +3,69 @@ const course = {
   parts: [
     {
       name: 'Fundamentals of React',
-      exercises: 10
+      exercises: 10,
     },
     {
       name: 'Using props to pass data',
-      exercises: 7
+      exercises: 7,
     },
     {
       name: 'State of a component',
-      exercises: 14
-    }
-  ]
-}
-  const Header = (props) => {
-    console.log(props);
-    return (
-      <>
-        <h1>{props.course}</h1>
-      </>
-    );
-  };
+      exercises: 14,
+    },
+  ],
+};
+const Header = (props) => {
+  console.log(props);
+  return (
+    <>
+      <h1>{props.course}</h1>
+    </>
+  );
+};
 
-  const Part = (props) => {
-    console.log(props);
-    return (
-      <>
-        <p>
-          {props.part.name}
-          {props.part.exercises}
-        </p>
-      </>
-    );
-  };
+const Part = (props) => {
+  console.log(props);
+  return (
+    <>
+      <p>
+        {props.part.name}
+        {props.part.exercises}
+      </p>
+    </>
+  );
+};
 
-  const Content = (props) => {
-    return (
-      <>
-    <Part part={props.parts[0]} />
-    <Part part={props.parts[1]} />
-    <Part part={props.parts[2]} />
-      </>
-    );
-  };
+const Content = (props) => {
+  console.log(props);
+  return (
+    <>
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
+    </>
+  );
+};
 
-  const Total = (props) => {
-    return (
-      <>
-        <p>Number of exercises {}</p>
-      </>
-    );
-  };
-  const App = () => {
+const Total = (props) => {
+  console.log(props);
+  let sum = 0;
+  for (const part of props.course.parts) {
+    sum += part.exercises;
+  }
+
+  return (
+    <>
+      <p>Number of exercises {sum}</p>
+    </>
+  );
+};
+const App = () => {
   return (
     <>
       <Header course={course.name} />
-      <Content parts={course.parts}/>
-      <Total />
+      <Content parts={course.parts} />
+      <Total course = {course}/>
     </>
   );
 };
