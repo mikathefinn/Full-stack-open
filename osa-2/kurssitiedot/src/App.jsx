be280d86f59cfa8012d107e498810fe6ac846ec5
mikathefinn <mikathefinn@gmail.com>
2023-10-24 11:46:55 +0300

    2.1 done

---
+++ osa-2/kurssitiedot/src/App.jsx
@@ -0,0 +1,50 @@
+const Header = ({ name }) => {
+  return <h1>{name}</h1>;
+};
+const Part = ({ part }) => {
+  return <p>{part.name} {part.exercises}</p>
+};
+
+const Course = ({ course }) => {
+  console.log(course);
+  return (
+    <div>
+      <Header name={course.name} />
+      {course.parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
+    </div>
+  );
+};
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  );
+};
+
+export default App;
