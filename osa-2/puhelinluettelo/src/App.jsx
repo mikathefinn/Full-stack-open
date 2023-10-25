1c29658921484a89ea18eaed82c437b0c50971eb
mikathefinn <mikathefinn@gmail.com>
2023-10-25 08:34:39 +0300

    2.9 done

--- osa-2/puhelinluettelo/src/App.jsx
+++ osa-2/puhelinluettelo/src/App.jsx
@@ -2,9 +2,15 @@ import { useState } from 'react';
 import Person from './components/Person';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' },
+  ]);
   const [newName, setNewName] = useState('');
-  const [newNumber, setNewNumber] = useState('')
+  const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -21,7 +27,7 @@ const App = () => {
       };
       setPersons(persons.concat(personObj));
       setNewName('');
-      setNewNumber('')
+      setNewNumber('');
     }
   };
 
@@ -29,12 +35,27 @@ const App = () => {
     console.log(event.target.value);
     setNewName(event.target.value);
   };
-const handleNumberChange = (event) =>{
-  setNewNumber(event.target.value)
-}
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+  const handleFiltering = (event) => {
+    setFilter(event.target.value);
+  };
+  const filteredNames = persons.filter((person) =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  );
+  console.log(filteredNames);
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+        <div>
+          filter with: <input value={filter} onChange={handleFiltering} />
+        </div>
+      </form>
+
+      <h2>Add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -47,7 +68,7 @@ const handleNumberChange = (event) =>{
         </div>
       </form>
       <h2>Numbers</h2>
-      <Person persons={persons} />
+      <Person persons={filteredNames} />
     </div>
   );
 };
